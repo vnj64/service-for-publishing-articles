a57d0f0c07e456cbb5927bf2329d0b090f15da77
vnJ <kamil_khabibullin@bk.ru>
2023-10-21 12:24:42 +0300

    hello repository

---
+++ src/main/java/com/example/publishingservice/services/ArticleService.java
@@ -0,0 +1,16 @@
+package com.example.publishingservice.services;
+
+import com.example.publishingservice.models.Article;
+import com.example.publishingservice.repositories.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public Article createArticles(Article article) {
+        return articleRepository.save();
+    }
+}

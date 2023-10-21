888dc88cded74b247071bc6efc0a8a04027f4a11
vnJ <kamil_khabibullin@bk.ru>
2023-10-21 18:17:41 +0300

    deleted directories

--- src/main/java/com/example/publishingservice/repositories/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.publishingservice.repositories;
-
-import com.example.publishingservice.models.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}

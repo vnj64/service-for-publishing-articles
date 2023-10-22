59133ccdaac2d4cd4f8b2afa3f4f9db68fdc6d49
ZakharEvv <zuevzahar9164@gmail.com>
2023-10-22 07:34:06 +0300

    Database connection

--- src/main/java/com/example/publishingservice/configuration/CorsConfig.java
+++ src/main/java/com/example/publishingservice/configuration/CorsConfig.java
@@ -2,17 +2,13 @@ package com.example.publishingservice.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
+public class CorsConfig extends WebMvcConfigurationSupport {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Позволяет CORS для всех URL
-                .allowedOrigins("https://inkwell-five.vercel.app:8080", "http://localhost:8080", "http://212.193.62.200:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true)
-                .maxAge(3600);
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*")
+                .allowCredentials(true);
     }
 }

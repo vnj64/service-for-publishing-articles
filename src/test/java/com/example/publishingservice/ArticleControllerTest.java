9864eceb4c970e3f8e852b2efe9df577fb2e72c8
vnJ <kamil_khabibullin@bk.ru>
2023-10-22 03:12:52 +0300

    fix: articleTheme field deleted from tests

--- src/test/java/com/example/publishingservice/ArticleControllerTest.java
+++ src/test/java/com/example/publishingservice/ArticleControllerTest.java
@@ -32,7 +32,7 @@ public class ArticleControllerTest {
     }
     @Test
     public void testCreateArticle() throws Exception {
-        ArticleDTO articleDTO = new ArticleDTO("Test", "Hello", "Name", "23-10", "Body", "Theme", "Theme1");
+        ArticleDTO articleDTO = new ArticleDTO("Test", "Hello", "Name", "23-10", "Body", "Theme");
         String articleJson = new ObjectMapper().writeValueAsString(articleDTO);
 
         mockMvc.perform(MockMvcRequestBuilders.post("api/article")
